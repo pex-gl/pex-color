20066fdec3da1ab9768c1c0c4232916d15138f6d
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-10-15 10:42:26 +0200

    added fromHex() and toHex() methods

--- examples/color.Color.js
+++ examples/color.Color.js
@@ -10,3 +10,7 @@ var c3 = new Color();
 c3.setHSV(0.95, 0.5, 0.5);
 console.log(c3);
 console.log(c3.getHSV());
+
+var c4 = new Color.fromHex("#FFAA00");
+console.log(c4);
+console.log(c4.getHex());
