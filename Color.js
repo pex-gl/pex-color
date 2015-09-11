8b74bec6c743e2dcc71c3c93e271776ae86496e6
Marcin Ignac <marcin.ignac@gmail.com>
2015-09-11 12:04:11 +0100

    Ported all RGB, HSV, HSL and added tests

--- Color.js
+++ Color.js
@@ -19,95 +19,82 @@ var lerp = require('lerp');
 //`a` - alpha opacity *{ Number 0..1 }* = 1
 function create(r, g, b, a) {
   return [r || 0, g || 0, b || 0, (a === undefined) ? 1 : a];
-};
+}
+
+//### copy()
+//Copies rgba values from another color into this instance
+//`c` - another color to copy values from *{ Color }*
+function copy(color, out) {
+    if (out !== undefined) {
+        out[0] = color[0];
+        out[1] = color[1];
+        out[2] = color[2];
+        out[3] = color[3];
+        return out;
+    }
+    return color.slice(0);
+}
 
 //### fromRGB(r, g, b, a)
 //Alias for create(r, g, b, a)
-
 function fromRGB(r, g, b, a) {
     return create(r, g, b, a);
 }
-
-//### fromByteArray(a)
-//Creates new color from array of 4 byte values [r, g, b, a]
-//`a` - array of rgba values *{ Array of Numbers/Int 0..255 }* = [0, 0, 0, 255]
-/*
-Color.fromByteArray = function(a) {
- return [a[0]/255, a[1]/255, a[2]/255, (a.length == 4) ? a[3]/255 : 255];
-};
-*/
-
-/*
-//### fromHSV(h, s, v, a)
-//Creates new color from hue, saturation and value
-//`h` - hue *{ Number 0..1 }* = 0
-//`s` - saturation *{ Number 0..1 }* = 0
-//`v` - value *{ Number 0..1 }* = 0
-//`a` - alpha opacity *{ Number 0..1 }* = 1
-Color.fromHSV = function(h, s, v, a) {
-    var c = c
-  var c = new Color();
-  c.setHSV(h, s, v, a);
-  return c;
-};
-*/
-//### fromHSL(h, s, l, a)
-//Creates new color from hue, saturation and lightness
-//`h` - hue *{ Number 0..1 }* = 0
-//`s` - saturation *{ Number 0..1 }* = 0
-//`l` - lightness *{ Number 0..1 }* = 0
-//`a` - alpha opacity *{ Number 0..1 }* = 1
-function fromHSL(h, s, l, a) {
-  var c = create();
-  setHSL(c, h, s, l, a);
-  return c;
-};
-
-//### fromHex(hex)
-//Creates new color from html hex value e.g. #FF0000
-//`hex` - html hex color string (with or without #) *{ String }*
-function fromHex(hex) {
-  var c = create();
-  setHex(c, hex);
-  return c;
-};
-
-/*
-//### fromXYZ(x, y, z)
-//Creates new color from XYZ representation
-//x - *{ Number 0..1 }*
-//y - *{ Number 0..1 }*
-//z - *{ Number 0..1 }*
-Color.fromXYZ = function(x, y, z) {
-  var c = new Color();
-  c.setXYZ(x, y, z);
-  return c;
-};
-
-//### fromLab(l, a, b)
-//Creates new color from Lab representation
-//l - *{ Number 0..100 }*
-//a - *{ Number -128..127 }*
-//b - *{ Number -128..127 }*
-Color.fromLab = function(l, a, b) {
-  var c = new Color();
-  c.setLab(l, a, b);
-  return c;
-};
 
 //### set(r, g, b, a)
 //`r` - red component *{ Number 0..1 }* = 0
 //`g` - green component *{ Number 0..1 }* = 0
 //`b` - blue component *{ Number 0..1 }* = 0
 //`a` - alpha opacity *{ Number 0..1 }* = 1
-Color.prototype.set = function(r, g, b, a) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
-  this.a = (a !== undefined) ? a : 1;
+function set(color, r, g, b, a) {
+  color[0] = r;
+  color[1] = g;
+  color[2] = b;
+  color[3] = (a !== undefined) ? a : 1;
 
-  return this;
-};
+  return color;
+}
+
+//### setRGB(r, g, b, a)
+//`r` - red component *{ Number 0..1 }* = 0
+//`g` - green component *{ Number 0..1 }* = 0
+//`b` - blue component *{ Number 0..1 }* = 0
+//`a` - alpha opacity *{ Number 0..1 }* = 1
+function setRGB(color, r, g, b, a) {
+    color[0] = r;
+    color[1] = g;
+    color[2] = b;
+    color[3] = (a !== undefined) ? a : 1;
+
+    return color;
+}
+
+//### fromRGBBytes(a)
+//Creates new color from array of 4 byte values [r, g, b, a]
+//`a` - array of rgba values *{ Array of Numbers/Int 0..255 }* = [0, 0, 0, 255]
+function fromRGBBytes(bytes) {
+    return [ bytes[0]/255, bytes[1]/255, bytes[2]/255, (bytes.length == 4) ? bytes[3]/255 : 1];
+}
+
+function getRGBBytes(color, out) {
+    out = out || [0, 0, 0];
+    out[0] = Math.floor(color[0]*255);
+    out[1] = Math.floor(color[1]*255);
+    out[2] = Math.floor(color[2]*255);
+    return out;
+}
+
+//### fromHSV(h, s, v, a)
+//Creates new color from hue, saturation and value
+//`h` - hue *{ Number 0..1 }* = 0
+//`s` - saturation *{ Number 0..1 }* = 0
+//`v` - value *{ Number 0..1 }* = 0
+//`a` - alpha opacity *{ Number 0..1 }* = 1
+function fromHSV(h, s, v, a) {
+  var color = create();
+  setHSV(color, h, s, v, a)
+  return color;
+}
 
 //### setHSV(h, s, l, a)
 //Sets rgb color values from a hue, saturation, value and alpha
@@ -115,7 +102,7 @@ Color.prototype.set = function(r, g, b, a) {
 //`s` - saturation *{ Number 0..1 }* = 0
 //`v` - value *{ Number 0..1 }* = 0
 //`a` - alpha opacity *{ Number 0..1 }* = 1
-Color.prototype.setHSV = function(h, s, v, a) {
+function setHSV(color, h, s, v, a) {
   a = a || 1;
 
   var i = Math.floor(h * 6);
@@ -125,25 +112,25 @@ Color.prototype.setHSV = function(h, s, v, a) {
   var t = v * (1 - (1 - f) * s);
 
   switch (i % 6) {
-    case 0: this.r = v; this.g = t; this.b = p; break;
-    case 1: this.r = q; this.g = v; this.b = p; break;
-    case 2: this.r = p; this.g = v; this.b = t; break;
-    case 3: this.r = p; this.g = q; this.b = v; break;
-    case 4: this.r = t; this.g = p; this.b = v; break;
-    case 5: this.r = v; this.g = p; this.b = q; break;
+    case 0: color[0] = v; color[1] = t; color[2] = p; break;
+    case 1: color[0] = q; color[1] = v; color[2] = p; break;
+    case 2: color[0] = p; color[1] = v; color[2] = t; break;
+    case 3: color[0] = p; color[1] = q; color[2] = v; break;
+    case 4: color[0] = t; color[1] = p; color[2] = v; break;
+    case 5: color[0] = v; color[1] = p; color[2] = q; break;
   }
 
-  this.a = a;
-  return this;
-};
+  color[3] = a;
+  return color;
+}
 
 //### getHSV()
 //Returns hue, saturation, value and alpha of color as
 //*{ Object h:0.1, s:0..1, v:0..1, a:0..1 }*
-Color.prototype.getHSV = function() {
-  var r = this.r;
-  var g = this.g;
-  var b = this.b;
+function getHSV(color) {
+  var r = color[0];
+  var g = color[1];
+  var b = color[2];
   var max = Math.max(r, g, b);
   var min = Math.min(r, g, b);
   var h;
@@ -163,9 +150,54 @@ Color.prototype.getHSV = function() {
     h /= 6;
   }
 
-  return { h: h, s: s, v: v, a: this.a };
-};
-*/
+  return [ h, s, v, color[3] ];
+}
+
+//### fromHSL(h, s, l, a)
+//Creates new color from hue, saturation and lightness
+//`h` - hue *{ Number 0..1 }* = 0
+//`s` - saturation *{ Number 0..1 }* = 0
+//`l` - lightness *{ Number 0..1 }* = 0
+//`a` - alpha opacity *{ Number 0..1 }* = 1
+function fromHSL(h, s, l, a) {
+  var color = create();
+  setHSL(color, h, s, l, a);
+  return color;
+}
+
+//### fromHex(hex)
+//Creates new color from html hex value e.g. #FF0000
+//`hex` - html hex color string (with or without #) *{ String }*
+function fromHex(hex) {
+  var color = create();
+  setHex(color, hex);
+  return color;
+}
+
+//### fromXYZ(x, y, z)
+//Creates new color from XYZ representation
+//x - *{ Number 0..1 }*
+//y - *{ Number 0..1 }*
+//z - *{ Number 0..1 }*
+function fromXYZ(x, y, z) {
+  var color = create();
+  setXYZ(color, x, y, z);
+  return color;
+}
+
+//### fromLab(l, a, b)
+//Creates new color from Lab representation
+//l - *{ Number 0..100 }*
+//a - *{ Number -128..127 }*
+//b - *{ Number -128..127 }*
+function fromLab(l, a, b) {
+  var color = create();
+  setLab(color, l, a, b);
+  return color;
+}
+
+
+
 //### setHSL(h, s, l, a)
 //Sets rgb color values from a hue, saturation, lightness and alpha
 //`h` - hue *{ Number 0..1 }* = 0
@@ -173,7 +205,7 @@ Color.prototype.getHSV = function() {
 //`l` - lightness *{ Number 0..1 }* = 0
 //`a` - alpha opacity *{ Number 0..1 }* = 1
 //Based on [https://gist.github.com/mjijackson/5311256](https://gist.github.com/mjijackson/5311256)
-function setHSL(c, h, s, l, a) {
+function setHSL(color, h, s, l, a) {
     a = a || 1;
 
     function hue2rgb(p, q, t) {
@@ -187,29 +219,28 @@ function setHSL(c, h, s, l, a) {
 
 
     if (s === 0) {
-        c[0] = c[1] = c[2] = l; // achromatic
+        color[0] = color[1] = color[2] = l; // achromatic
     }
     else {
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
-        c[0] = hue2rgb(p, q, h + 1/3);
-        c[1] = hue2rgb(p, q, h);
-        c[2] = hue2rgb(p, q, h - 1/3);
+        color[0] = hue2rgb(p, q, h + 1/3);
+        color[1] = hue2rgb(p, q, h);
+        color[2] = hue2rgb(p, q, h - 1/3);
     }
 
-    c[3] = a;
-    return c;
+    color[3] = a;
+    return color;
 }
 
-/*
 //### getHSL()
 //Returns hue, saturation, lightness and alpha of color as
 //*{ Object h:0.1, s:0..1, l:0..1, a:0..1 }*
 //Based on [https://gist.github.com/mjijackson/5311256](https://gist.github.com/mjijackson/5311256)
-Color.prototype.getHSL = function() {
-  var r = this.r;
-  var g = this.g;
-  var b = this.b;
+function getHSL(color) {
+  var r = color[0];
+  var g = color[1];
+  var b = color[2];
   var max = Math.max(r, g, b);
   var min = Math.min(r, g, b);
   var l = (max + min) / 2;
@@ -232,50 +263,49 @@ Color.prototype.getHSL = function() {
     h /= 6;
   }
 
-  return { h: h, s: s, l: l, a: this.a };
-};
-*/
+  return  [ h, s, l, color[3] ];
+}
+
 //### setHex(hex)
 //Sets rgb color values from a html hex value e.g. #FF0000
 //`hex` - html hex color string (with or without #) *{ String }*
-function setHex(c, hex) {
+function setHex(color, hex) {
     hex = hex.replace(/^#/, "");
     var num = parseInt(hex, 16);
 
-    c[0] = (num >> 16 & 255) / 255;
-    c[1] = (num >> 8 & 255) / 255;
-    c[2] = (num & 255) / 255;
-    c[3] = 1
+    color[0] = (num >> 16 & 255) / 255;
+    color[1] = (num >> 8 & 255) / 255;
+    color[2] = (num & 255) / 255;
+    color[3] = 1
 
-    return c;
-};
+    return color;
+}
 
-/*
 //### getHex()
 //Returns html hex representation of this color *{ String }*
-Color.prototype.getHex = function() {
-  var color = [ this.r, this.g, this.b ].map(function(val) {
+function getHex(color) {
+  var c = [ color[0], color[1], color[2] ].map(function(val) {
     return Math.floor(val * 255);
   });
 
-  return "#" + ((color[2] | color[1] << 8 | color[0] << 16) | 1 << 24)
+  return "#" + ((c[2] | c[1] << 8 | c[0] << 16) | 1 << 24)
     .toString(16)
     .slice(1)
     .toUpperCase();
-};
+}
 
-
+/*
 //### setXYZ(x, y, z)
 //Sets rgb color values from XYZ
 //x - *{ Number 0..1 }*
 //y - *{ Number 0..1 }*
 //z - *{ Number 0..1 }*
-Color.prototype.setXYZ = function(x, y, z) {
+function setXYZ(colo,r x, y, z) {
   var rgb = {
     r: x *  3.2406 + y * -1.5372 + z * -0.4986,
     g: x * -0.9689 + y *  1.8758 + z *  0.0415,
     b: x *  0.0557 + y * -0.2040 + z *  1.0570
-  };
+  }
 
   [ "r", "g", "b" ].forEach(function(key) {
     rgb[key] /= 100;
@@ -292,18 +322,18 @@ Color.prototype.setXYZ = function(x, y, z) {
     }
   });
 
-  this.r = rgb.r;
-  this.g = rgb.g;
-  this.b = rgb.b;
-  this.a = 1.0;
+  color[0] = rgb.r;
+  color[1] = rgb.g;
+  color[2] = rgb.b;
+  color[3] = 1.0;
 
-  return this;
-};
+  return color;
+}
 
 //### getXYZ()
 //Returns xyz representation of this color as
 //*{ Object x:0..1, y:0..1, z:0..1 }*
-Color.prototype.getXYZ = function() {
+function getXYZ(color) {
   var rgb = this.clone();
 
   [ "r", "g", "b" ].forEach(function(key) {
@@ -320,20 +350,20 @@ Color.prototype.getXYZ = function() {
     x: rgb.r * 0.4124 + rgb.g * 0.3576 + rgb.b * 0.1805,
     y: rgb.r * 0.2126 + rgb.g * 0.7152 + rgb.b * 0.0722,
     z: rgb.r * 0.0193 + rgb.g * 0.1192 + rgb.b * 0.9505
-  };
-};
+  }
+}
 
 //### setLab(l, a, b)
 //Sets rgb color values from Lab
 //l - *{ Number 0..100 }*
 //a - *{ Number -128..127 }*
 //b - *{ Number -128..127 }*
-Color.prototype.setLab = function(l, a, b) {
+function setLab(color, l, a, b) {
   var y = (l + 16) / 116;
   var x = a / 500 + y;
   var z = y - b / 200;
 
-  var xyz = { x: x, y: y, z: z };
+  var xyz = { x: x, y: y, z: z }
   var pow;
 
   [ "x", "y", "z" ].forEach(function(key) {
@@ -349,20 +379,20 @@ Color.prototype.setLab = function(l, a, b) {
 
   var color = Color.fromXYZ(xyz.x, xyz.y, xyz.z);
 
-  this.r = color.r;
-  this.g = color.g;
-  this.b = color.b;
-  this.a = color.a;
+  color[0] = color.r;
+  color[1] = color.g;
+  color[2] = color.b;
+  color[3] = color.a;
 
-  return this;
-};
+  return color;
+}
 
 //### getLab()
 //Returns Lab representation of this color as
 //*{ Object l: 0..100, a: -128..127, b: -128..127 }*
-Color.prototype.getLab = function() {
-  var white = { x: 95.047, y: 100.000, z: 108.883 };
-  var xyz = this.getXYZ();
+function getLab(color) {
+  var white = { x: 95.047, y: 100.000, z: 108.883 }
+  var xyz = color[1]etXYZ();
 
   [ "x", "y", "z" ].forEach(function(key) {
     xyz[key] /= white[key];
@@ -379,38 +409,14 @@ Color.prototype.getLab = function() {
     l: 116 * xyz.y - 16,
     a: 500 * (xyz.x - xyz.y),
     b: 200 * (xyz.y - xyz.z)
-  };
-};
-
-//### copy()
-//Copies rgba values from another color into this instance
-//`c` - another color to copy values from *{ Color }*
-Color.prototype.copy = function(c) {
-  this.r = c.r;
-  this.g = c.g;
-  this.b = c.b;
-  this.a = c.a;
-
-  return this;
-};
-
-//### clone()
-//Returns a copy of this color *{ Color }*
-Color.prototype.clone = function() {
-  return new Color(this.r, this.g, this.b, this.a);
-};
-
-//### hash()
-//Returns one (naive) hash number representation of this color *{ Number }*
-Color.prototype.hash = function() {
-  return 1 * this.r + 12 * this.g + 123 * this.b + 1234 * this.a;
-};
+  }
+}
 
 //### distance(color)
 //Returns distance (CIE76) between this and given color using Lab representation *{ Number }*
 //Based on [http://en.wikipedia.org/wiki/Color_difference](http://en.wikipedia.org/wiki/Color_difference)
-Color.prototype.distance = function(color) {
-  var lab1 = this.getLab();
+function distance(color) {
+  var lab1 = color.getLab();
   var lab2 = color.getLab();
 
   var dl = lab2.l - lab1.l;
@@ -418,7 +424,7 @@ Color.prototype.distance = function(color) {
   var db = lab2.b - lab1.b;
 
   return Math.sqrt(dl * dl, da * da, db * db);
-};
+}
 
 //### lerp(startColor, endColor, t, mode)
 //Creates new color from linearly interpolated two colors
@@ -426,7 +432,7 @@ Color.prototype.distance = function(color) {
 //`endColor` - *{ Color } *
 //`t` - interpolation ratio *{ Number 0..1 }*
 //`mode` - interpolation mode : 'rgb', 'hsv', 'hsl' *{ String }* = 'rgb'
-Color.lerp = function(startColor, endColor, t, mode) {
+function lerp(startColor, endColor, t, mode) {
   mode = mode || 'rgb';
 
   if (mode === 'rgb') {
@@ -460,16 +466,31 @@ Color.lerp = function(startColor, endColor, t, mode) {
   else {
     return startColor;
   }
-};
+}
 */
 
 var Color = {
-    create  : create,
-    fromRGB : fromRGB,
-    fromHSL : fromHSL,
-    setHSL  : setHSL,
-    fromHex : fromHex,
-    setHex  : setHex,
+    create   : create,
+    copy     : copy,
+
+    fromRGB  : fromRGB,
+    setRGB   : setRGB,
+    set      : set,
+
+    fromRGBBytes: fromRGBBytes,
+    getRGBBytes : getRGBBytes,
+
+    fromHSV  : fromHSV,
+    setHSV   : setHSV,
+    getHSV   : getHSV,
+
+    fromHSL  : fromHSL,
+    setHSL   : setHSL,
+    getHSL   : getHSL,
+
+    fromHex  : fromHex,
+    setHex   : setHex,
+    getHex   : getHex,
 
     //Predefined colors ready to use
     Transparent : [0, 0, 0, 0],
--- test/Color.js
+++ test/Color.js
@@ -1,18 +1,58 @@
 var assert = require('assert');
 var Color = require('../Color');
 
-assert.deepEqual(Color.create(0.1, 0.2, 0.3, 0.4), [0.1, 0.2, 0.3, 0.4]);
+var EPSILON = 0.0000001;
 
+function assertDeepAlmostEqual(a, b) {
+    if (a.length != b.length) {
+        throw new Error(a + ' ' + 'assertDeepAlmostEqual' + ' ' + b);
+    }
+    for(var i=0; i<a.length; i++) {
+        if (Math.abs(a[i] - b[i]) > EPSILON) {
+            throw new Error(a + ' ' + 'assertDeepAlmostEqual' + ' ' + b);
+        }
+    }
+    return true;
+}
+
+assert.deepEqual(Color.create(0.1, 0.2, 0.3, 0.4), [0.1, 0.2, 0.3, 0.4]);
 assert.deepEqual(Color.create(0.1, 0.2, 0.3), [0.1, 0.2, 0.3, 1.0]);
 
+assert.deepEqual(Color.fromRGB(0.1, 0.2, 0.3), [0.1, 0.2, 0.3, 1.0]);
 assert.deepEqual(Color.fromRGB(0.1, 0.2, 0.3, 0.4), [0.1, 0.2, 0.3, 0.4]);
 
+assert.deepEqual(Color.setRGB([0, 0, 0], 0.1, 0.2, 0.3), [0.1, 0.2, 0.3, 1.0]);
+assert.deepEqual(Color.setRGB([0, 0, 0], 0.1, 0.2, 0.3, 0.4), [0.1, 0.2, 0.3, 0.4]);
+
+assert.deepEqual(Color.set([0, 0, 0], 0.1, 0.2, 0.3), [0.1, 0.2, 0.3, 1.0]);
+assert.deepEqual(Color.set([0, 0, 0], 0.1, 0.2, 0.3, 0.4), [0.1, 0.2, 0.3, 0.4]);
+
+assert.deepEqual(Color.fromRGBBytes([222, 100, 125]), [222/255, 100/255, 125/255, 1.0]);
+assert.deepEqual(Color.fromRGBBytes([222, 100, 125, 23]), [222/255, 100/255, 125/255, 23/255]);
+
+assert.deepEqual(Color.getRGBBytes([222/255, 100/255, 125/255, 23/25]), [222, 100, 125]);
+
+assert.deepEqual(Color.fromHSV(0.0, 1.0, 1.0), [1.0, 0.0, 0.0, 1.0])
+assert.deepEqual(Color.fromHSV(0.0, 0.5, 1.0), [1.0, 0.5, 0.5, 1.0])
+assertDeepAlmostEqual(Color.fromHSV(0.333333333333, 1.0, 1.0), [0.0, 1.0, 0, 1.0])
+assertDeepAlmostEqual(Color.fromHSV(0.333333333333, 0.5, 0.5), [0.25, 0.5, 0.25, 1.0])
+assertDeepAlmostEqual(Color.setHSV([0,0,0,1], 0.333333333333, 0.5, 0.5), [0.25, 0.5, 0.25, 1.0])
+assertDeepAlmostEqual(Color.getHSV([1.0, 0.0, 0.0, 1.0]), [0.0, 1.0, 1.0, 1.0])
+assertDeepAlmostEqual(Color.getHSV([0.25, 0.5, 0.25, 0.5]), [0.333333333333, 0.5, 0.5, 0.5])
+
 assert.deepEqual(Color.fromHSL(0, 1, 0.5), [1, 0, 0, 1])
+assertDeepAlmostEqual(Color.fromHSL(0.333333333333, 1, 0.5), [0, 1, 0, 1])
 assert.deepEqual(Color.fromHSL(0.5, 1, 0.5), [0,0.9999999999999998,1,1])
 assert.deepEqual(Color.setHSL([0, 0, 0, 1], 0, 1, 0.5), [1, 0, 0, 1]);
+assert.deepEqual(Color.getHSL([1, 0, 0, 1]), [0, 1, 0.5, 1.0]);
 
 assert.deepEqual(Color.fromHex("#FF0000"), [1, 0, 0, 1]);
 assert.deepEqual(Color.fromHex("#00FF00"), [0, 1, 0, 1]);
 assert.deepEqual(Color.fromHex("#0000FF"), [0, 0, 1, 1]);
-
+assert.deepEqual(Color.fromHex("#FF0066"), [1, 0, 0.4, 1]);
 assert.deepEqual(Color.setHex([0, 0, 0, 1], "#FF00FF"), [1, 0, 1, 1]);
+assert.deepEqual(Color.getHex([1, 0, 0.4, 1]), "#FF0066");
+
+assert.deepEqual(Color.copy([0.1, 0.2, 0.3, 0.4]), [0.1, 0.2, 0.3, 0.4]);
+
+console.log('Done')
